02dde4fd33cea3306f055d1d104c5f6311c780ac
Lucas <lucasbiazonpalma@gmail.com>
2024-03-06 22:56:07 -0300

    Organização das pastas

--- src/app.ts
+++ src/app.ts
@@ -6,12 +6,15 @@ const fastify = Fastify({
 
 
 fastify.get('/', function (request, reply) {
-  reply.send({ hello: 'world' })
+  reply.send(
+    { hello: 'world' }
+  )
 })
 
-fastify.listen({ port: 3000 }, function (err, address) {
+fastify.listen({ port: 3000}, function (err, address) {
   if (err) {
     fastify.log.error(err)
     process.exit(1)
   }
+  console.log('Run in: ' + address)
 })
