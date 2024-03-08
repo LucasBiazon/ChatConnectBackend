4f72bdd5dedbd0f3470ac60b7064171b932af0ea
Lucas <lucasbiazonpalma@gmail.com>
2024-03-07 23:35:00 -0300

    Removendo user, vou fazer sem copiar hehe

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,37 +1,5 @@
 import { randomUUID } from "crypto";
 import { Replace } from "../helpers/replace";
-export interface UserProps {
-  id: string;
-  name: string;
-  email: string;
-  password: string;
-  telephone: string;
-  profileImg: string;
-  biography:  string;
-  birthday: Date;
-  status?: string | null;
-  created_at?: Date;
-  updated_at?: Date | null;
-}
 
-export class User {
-  private _id: string;
-  private props: UserProps;
-  costructor(
-    props: Replace<UserProps, { createdAt?: Date }>,
-    id?: string,
-  ){
-    this._id = randomUUID();
-    this.props = {
-      ...props,
-      created_at: props.created_at || new Date(),
-      updated_at: props.updated_at || new Date(),
-    };
-  }
+export class User {}
 
-  public get id(): string{
-    return this._id;
-  }
-
-
-}
